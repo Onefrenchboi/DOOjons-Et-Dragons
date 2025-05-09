104b50673b6eae7371f5d7d2ba7fa516d76870c2
Mathéo Faucon <fcnmatheo@gmail.com>
2025-05-09 19:07:06 +0200

    Add attributes to Character class, create Statistics class, update uml to match

--- src/Game/Entities/Character.java
+++ src/Game/Entities/Character.java
@@ -1,4 +1,19 @@
 package Game.Entities;
 
+import Game.Entities.Races.*;
+import Game.Entities.Classes.*;
+import Game.Equipment.*;
+
+import java.util.List;
+
 public class Character {
+    private String _name;
+    private Race _race;
+    private CharacterClass _class;
+    private Statistics _stats;
+    private List<Equipment> _equipment;
+    private Weapon _equippedWeapon;
+    private Armor _equippedArmor;
+
+
 }
---
+++ src/Game/Entities/Statistics.java
@@ -0,0 +1,9 @@
+package Game.Entities;
+
+public class Statistics {
+    private int _hp;
+    private int _strength;
+    private int _dexterity;
+    private int _speed;
+    private int _initiative;
+}
