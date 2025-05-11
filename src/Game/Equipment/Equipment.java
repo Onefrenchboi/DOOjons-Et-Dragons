35e4f409dfdc083822ad19bf1b9fbcd898dcd1c2
Mathéo Faucon <fcnmatheo@gmail.com>
2025-05-11 10:33:48 +0200

    Add attributes and getter of Equipment class (yes I woke up 5 hours after my last commit just to do this). Updated UML to match

--- src/Game/Equipment/Equipment.java
+++ src/Game/Equipment/Equipment.java
@@ -3,4 +3,13 @@ package Game.Equipment;
 public abstract class Equipment {
     private String _name;
 
+    public Equipment(String name) {
+        _name = name;
+    }
+
+
+    public String get_name(){
+        return _name;
+    }
+
 }
